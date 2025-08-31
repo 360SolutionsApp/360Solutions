b66ced3bf989282aaeecb2a239f819b1ac0dd3d5
Armando Rubio <arman.2.r@gmail.com>
2025-08-31 18:06:28 -0500

    setup cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:8100',
+      'http://localhost',
+      'https://localhost',
+      'http://localhost:3000',
+      'http://localhost:4200',
+      'https://ingrubio.com',
+      'https://17gqklpf-3000.use.devtunnels.ms',
+      'capacitor://localhost',
+      'http://192.168.1.13:8100',
+    ], // O usa '*' para desarrollo (no recomendado para producción)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
